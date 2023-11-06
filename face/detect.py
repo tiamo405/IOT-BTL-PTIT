761a2759e4d2426ceabcce8da0f8642f1fc9afe5
tiamo405 <nam05052002@gmail.com>
2023-11-06 17:31:56 +0700

    def align, get emb

--- face/detect.py
+++ face/detect.py
@@ -1,12 +1,25 @@
 import cv2
 from mtcnn import MTCNN
 import numpy as np
+import os
+import sys
+root = os.getcwd()
+pwd = os.path.dirname(os.path.realpath("face"))
+sys.path.insert(0, root)
+from face import utils_face
+from face.face_infer import FaceEmbedding
+import config
 
+TRITON_HOST = config.TRITON_HOST
+TRITON_POST = config.TRITON_PORT
 
 class Face_Detect():
     def __init__(self, threshold_face = 0.5):
         self.model = MTCNN()
         self.threshold_face = threshold_face
+        self.face_embedding = FaceEmbedding(TRITON_HOST= TRITON_HOST, 
+                                            TRITON_PORT= TRITON_POST)
+
     def detect(self, cv_image):
         results = self.model.detect_faces(cv_image)
         bbox = []
@@ -23,7 +36,7 @@ class Face_Detect():
             bbox.append((x, y, w, h))
             dsts.append(dst)
             confidences.append(confidence)
-            return bbox, dsts, confidences
+        return bbox, dsts, confidences
     
     def _dst(self, keypoints ):
         left_eye = keypoints['left_eye']
@@ -38,5 +51,23 @@ class Face_Detect():
         dst.append(left_mouth)
         dst.append(right_mouth)
         return dst
+    
+    def align_face(self, cv_image, dst):
+        face_align = utils_face.align_face(cv_image, dst)
+        cv2.imwrite('face_align.jpg', face_align)
+        return face_align
+
+    def get_emb(self, cv_image, dst) :
+        face_align = self.align_face(cv_image, dst)
+        emb, score = self.face_embedding.get_emb(face_align)
+        return emb,score
+
+
+if __name__ == "__main__":  
+    Face_Detect_ = Face_Detect()
+    img = cv2.imread('debug.jpg')
+    bbox, dsts, confidences = Face_Detect_.detect(img)
+    dst = dsts[0]
+    Face_Detect_.align_face(img, dst)
     
     
