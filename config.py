aa6ceb3c98be927677c9587a84d03860e79265bf
tiamo405 <nam05052002@gmail.com>
2023-11-06 17:30:56 +0700

    export env

--- config.py
+++ config.py
@@ -1,2 +1,11 @@
-TRITON_HOST="103.130.57.57"
+TRITON_HOST="192.168.100.89"
 TRITON_PORT="8001"
+
+# ENDPOINT = os.environ.get('ENDPOINT')
+# ACCESS_KEY = os.environ.get('ACCESS_KEY')
+# SECRET_KEY = os.environ.get('SECRET_KEY')
+
+ENDPOINT = "192.168.100.254:9000"
+ACCESS_KEY = "hoangnt"
+SECRET_KEY = "cxview2023"
+SECURE = False
