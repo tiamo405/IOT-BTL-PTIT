4ced2ee314cf642881d1146a14764e9cea6c3c78
tiamo405 <nam05052002@gmail.com>
2023-11-08 16:16:47 +0700

    faiss train + nprobe

--- faiss_.py
+++ faiss_.py
@@ -4,15 +4,25 @@ import numpy as np
 from PIL import Image
 from s3_minio.minio_ import Minio_Client
 
-def faiss_search(emb, threshold= 0.5 , device= 'cpu', k = 1):
+def faiss_search(emb, threshold= 0.5 , device= 'cpu', k = 1, nprobe = 1, train = False):
   minio_client = Minio_Client()
   embs = minio_client.get_embs()
-  
+  dim = embs.shape[1]
   if device == 'cpu' :
-    model_faiss = faiss.IndexFlatL2(512)
+    model_faiss = faiss.IndexFlatL2(dim)
   else :
-    model_faiss = faiss.IndexFlatL2(512)
-  model_faiss.add(embs)
+    model_faiss = faiss.IndexFlatL2(dim)
+  if train :
+    model_faiss = faiss.IndexIVF(model_faiss, dim, 50)
+    model_faiss.train(embs)
+
+    model_faiss.is_trained
+
+    model_faiss.add(embs)
+    model_faiss.nprobe = nprobe
+  else :
+    model_faiss.add(embs)
+    
   D, I = model_faiss.search(emb, k)
   index = I[0][0]
   similarity_score = 1 / (1 + D[0][0])
