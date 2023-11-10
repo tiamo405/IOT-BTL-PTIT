20d22099daa6fb382eb877d8cb68c3016eca301a
tiamo405 <nam05052002@gmail.com>
2023-11-10 16:00:19 +0700

    doi bucket name

--- config.py
+++ config.py
@@ -7,12 +7,8 @@ env.read_env()
 TRITON_HOST = os.getenv("TRITON_HOST")
 TRITON_PORT = os.getenv("TRITON_PORT")
 
-# ENDPOINT = os.environ.get('ENDPOINT')
-# ACCESS_KEY = os.environ.get('ACCESS_KEY')
-# SECRET_KEY = os.environ.get('SECRET_KEY')
-
 ENDPOINT = "192.168.100.254:9000"
 ACCESS_KEY = "hoangnt"
 SECRET_KEY = "cxview2023"
 SECURE = False
-BUCKET = "84soft"
+BUCKET = "iot"
