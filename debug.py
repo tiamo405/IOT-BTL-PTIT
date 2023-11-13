5bee75c1a223e48dbee72cb08ae435abb0a17f83
tiamo405 <nam05052002@gmail.com>
2023-11-13 23:49:14 +0700

    nothing

--- debug.py
+++ debug.py
@@ -1,22 +1,3 @@
-import numpy as np
-import json
-from utils import datenow2timestamp
-
-data = []
-for i in range(4):
-    json_data = {
-        "customerID": i,
-        "timeVisit" : datenow2timestamp()
-    }
-    data.append(json_data)
-
-with open('tmp/data.json', 'w') as json_file:
-    json.dump(data, json_file)
-
-# Đọc dữ liệu từ tệp tin JSON
-with open('tmp/data.json', 'r') as json_file:
-    loaded_data = json.load(json_file)
-
-# Kiểm tra xem dữ liệu đã được đọc đúng chưa
-print(loaded_data)
-print(1)
+import os
+dir_root = os.path.dirname(os.path.abspath(__file__))
+print(dir_root)
