922aced24b81de987844382a5f16509a07176f6c
tiamo405 <nam05052002@gmail.com>
2023-11-16 00:56:50 +0700

    update key tele

--- config.py
+++ config.py
@@ -5,11 +5,24 @@ env.read_env()
 
 DIR_ROOT = os.path.dirname(os.path.abspath(__file__))
 
+
+# TRITON
 TRITON_HOST = os.getenv("TRITON_HOST")
 TRITON_PORT = os.getenv("TRITON_PORT")
 
+# S3
 ENDPOINT = "192.168.100.254:9000"
 ACCESS_KEY = "hoangnt"
 SECRET_KEY = "cxview2023"
 SECURE = False
 BUCKET = "iot"
+
+#Tele
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+USER_CHAT_ID = os.getenv("USER_CHAT_ID")
+
+#  threshold face
+THRESHOLD_FACE_DETECT = 0.5
+THRESHOLD_FACE_EMB = 0.3
+
+
