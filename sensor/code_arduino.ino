e3da8e1ee738c2e9cb9b90619ee8664dbedd4e5b
tiamo405 <nam05052002@gmail.com>
2023-11-20 01:12:42 +0700

    them code cua servo đóng mở

--- sensor/code_arduino.ino
+++ sensor/code_arduino.ino
@@ -1,4 +1,4 @@
-// #include<Servo.h>
+#include<Servo.h>
 #include<ESP8266WiFi.h>
 #include "FirebaseESP8266.h"
 // Provide the RTDB payload printing info and other helper functions.
@@ -21,15 +21,22 @@ String path = "/";
 FirebaseJson json;
 
 // dong co moter
-// Servo myServo;
+Servo myServo;
 
 unsigned long t1 = 0;
 // Biến để lưu giá trị từ Firebase
 String action;
-byte x = 0;
+
+
 void setup(){
+  // khởi tạo nơi in
   Serial.begin(115200);
+  // nút cắm động cơ
+  myServo.attach(D4);
+  myServo.write(0);
+  // kết nối wifi
   WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
+
   while(WiFi.status() != WL_CONNECTED){
     delay(5000);
     Serial.print(".");
@@ -40,26 +47,47 @@ void setup(){
   Serial.println();
 
   /* Assign the database URL(required) */
-    config.database_url = DATABASE_URL;
+  config.database_url = DATABASE_URL;
 
-    config.signer.test_mode = true;
-    // Comment or pass false value when WiFi reconnection will control by your code or third party library e.g. WiFiManager
-    Firebase.reconnectNetwork(true);
-    /* Initialize the library with the Firebase authen and config */
-    Firebase.begin(&config, &auth);
+  config.signer.test_mode = true;
+  // Comment or pass false value when WiFi reconnection will control by your code or third party library e.g. WiFiManager
+  Firebase.reconnectNetwork(true);
+  /* Initialize the library with the Firebase authen and config */
+  Firebase.begin(&config, &auth);
 
 }
 
+void quay(){
+  myServo.write(180);
+}
+
 void loop(){
-  Firebase.setString(firebaseData, path+"test", "Damocua");
+  // Firebase.setString(firebaseData, path+"action", "Damocua");
   
+  // if(millis()-t1 > 1000){
+  //   if(Firebase.getString(firebaseData, path + "action")){
+  //     action = firebaseData.stringData();
+  //     Serial.println(action);
+  //   }
+  //   t1 = millis();
+
+  // }
   if(millis()-t1 > 1000){
-    if(Firebase.getString(firebaseData, path + "test")){
+      if(Firebase.getString(firebaseData, path + "action")){
       action = firebaseData.stringData();
       Serial.println(action);
+      if(action == "mocua"){
+        quay();
+        Firebase.setString(firebaseData, path+"action", "damocua");
+      }
+      if(action == "dongcua"){
+        quay();
+        Firebase.setString(firebaseData, path+"action", "dadongcua");
+      }
     }
     t1 = millis();
-
   }
+  
+
 
 }
